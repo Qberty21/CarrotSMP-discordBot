461a590c76b1e6f1c279239606d21f7bb7363bf7
Denis <qberty21@gmail.com>
2022-02-27 22:11:29 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ bot = commands.Bot(command_prefix=settings['prefix'])
 @bot.command()
 async def hello(ctx): # Создаём функцию и передаём аргумент ctx.
     author = ctx.message.author #Объявляем переменную author и записываем туда информацию об авторе.
-    await ctx.send(f'Hello, {author.mention}!') # Выводим сообщение с упоминанием автора, обращаясь к переменной author.
+    await ctx.send(f'Kek, {author.mention}!') # Выводим сообщение с упоминанием автора, обращаясь к переменной author.
 
 bot.run(settings['token']) # Обращаемся к словарю settings с ключом token, для получения токена
